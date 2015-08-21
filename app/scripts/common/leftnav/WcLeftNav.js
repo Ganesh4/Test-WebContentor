df2bb5f6a66b8ad11e310b7abc3b5af961c5a2a9
subash0785 <subash@fusioninfosys.com>
2015-08-21 13:11:55 +0530

    Scripts Structure added

---
+++ app/scripts/common/leftnav/WcLeftNav.js
@@ -0,0 +1,26 @@
+/**
+ * Created by subhash on 16/7/15.
+ */
+'use strict';
+
+(function(angular){
+
+    angular.module('microsite').directive('wcLeftNav',function()
+        {
+                return{
+
+                    restrict:'AE',
+                    templateUrl:'./views/common/leftnav/leftnav.html',
+                    controller:('LeftnavCtrl',['$scope','$state'],function($scope,$state)
+                            {
+
+                                
+
+
+
+                            })
+
+
+                }
+        })
+     })(angular);
