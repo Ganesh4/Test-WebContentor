4596cfeac09651bcdbfa0374ed803b02c82a586b
subash0785 <subash@fusioninfosys.com>
2015-08-21 20:46:21 +0530

    Added Functionality of active class on left nav.

--- app/scripts/common/leftnav/WcLeftNav.js
+++ app/scripts/common/leftnav/WcLeftNav.js
@@ -5,22 +5,21 @@
 
 (function(angular){
 
-    angular.module('microsite').directive('wcLeftNav',function()
-        {
-                return{
+    angular.module('microsite').directive('wcLeftNav',function(){
+        return{
 
-                    restrict:'AE',
-                    templateUrl:'./views/commons/leftnav/leftnav.html',
-                    controller:('LeftnavCtrl',['$scope','$state'],function($scope,$state)
-                            {
+            restrict:'AE',
+            templateUrl:'./views/commons/leftnav/leftnav.html',
+            controller:('LeftnavCtrl',['$scope','$location','$state'],function($scope,$location,$state){
+                        
+                        $scope.isActive = function(route) {
+                        $scope.path = $location.path();
+                        console.log('test--------',route, $state.href($state.current.name) , $location.path() );
+                        return $state.is('home');
 
-                                
-
-
-
-                            })
-
-
-                }
-        })
-     })(angular);
+                };
+                      
+            })
+        }
+    })
+})(angular);
--- app/ui.js
+++ app/ui.js
@@ -12,13 +12,17 @@
     angular.module('microsite').config(['$stateProvider','$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
     //
     // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/overview");
+    $urlRouterProvider.otherwise("/home/overview");
     // Now set up the states
-    $stateProvider.state('overview', {
+    $stateProvider.state('home', {
+        url: "/home",
+        templateUrl:"views/overview/home.html"
+
+    }).state('home.overview', {
         url: "/overview",
         templateUrl:"views/overview/overview.html"
 
-    })
+    });
 }]);
 
 })(angular);
