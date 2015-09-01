a257d9b0df5d90e786ff34e0200294b74c156828
vipul031989 <vipul@webcontentor.com>
2015-09-01 14:15:36 +0530

    Ui js update for new states.

--- app/module-config.js
+++ app/module-config.js
@@ -4,16 +4,16 @@
  *
  *
  */
-	(function(angular){
+(function(angular){
 
 	angular.module('vendor',['ui.router','restangular']);
-	
+
 	angular.module('common',['vendor']);
 
-    angular.module('overview',['common']);
+	angular.module('overview',['common']);
 
- 	angular.module('microsite',['overview','common']);
+	angular.module('design',['vendor']);
 
+	angular.module('microsite',['overview','common']);
 
-	
- 	})(angular);
+})(angular);
