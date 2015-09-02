d670297916806124c855973e41f120be4b297653
vipul031989 <vipul@webcontentor.com>
2015-09-02 17:55:08 +0530

    Added Controller for design module.

---
+++ app/scripts/design/DesignApiSrv.js
@@ -0,0 +1,21 @@
+
+/**
+* 
+*
+*
+*/
+'use strict';
+
+(function(angular){
+angular.module('design').service('DesignSrv', ['$http', 'Restangular', function ($http , Restangular) {
+// Now set up the states
+	var self = this;
+	/**
+	Sends a post request to the backend.
+	**/
+	self.saveDesign = function(){
+		
+	}
+}]);
+
+})(angular);
