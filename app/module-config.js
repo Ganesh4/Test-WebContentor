a7b2fc50a15096843a57e55418a8026fa7c32ff8
subash0785 <subash@fusioninfosys.com>
2015-09-04 12:23:53 +0530

    moc

--- app/module-config.js
+++ app/module-config.js
@@ -11,10 +11,14 @@
 	angular.module('common',['vendor']);
 
 	angular.module('overview',['common']);
-    angular.module('design',['common']);
+    
 
 	angular.module('design',['common']);
 
-	angular.module('microsite',['overview','common','design']);
+	angular.module('header',['common']);
+
+	angular.module('subheader',['header']);
+
+	angular.module('microsite',['overview','common','design','header','subheader']);
 
 })(angular);
