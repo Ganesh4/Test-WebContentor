0eaca3899a8a94b490dbd6f4d66c3321783289cd
Ganesh4 <ughadeganesh@gmail.com>
2015-09-05 18:42:36 +0530

    Indentation

--- app/scripts/design/DesignApiSrv.js
+++ app/scripts/design/DesignApiSrv.js
@@ -1,4 +1,3 @@
-
 /**
 * 
 *
@@ -7,30 +6,33 @@
 'use strict';
 
 (function(angular){
-angular.module('design').service('DesignSrv', ['$http', 'Restangular', 'CommonSrv', function ($http , Restangular, CommonSrv) {
-// Now set up the states
-	var self = this;
-	/**
-		Sends a post request to the backend.
-	**/
-	self.saveDesign = function(userId, data, success, error){
-		var formData = CommonSrv.getFormData(data);
-		console.log('design ---------  ' , data);
-		
-		console.log(formData);
-		Restangular.one(userId.toString()).one('designs').withHttpConfig(
-			{
-				transformRequest: angular.identity
-			}).customPOST(formData, '', undefined, {
-				'Content-Type': undefined
-			}).then(success);
-	}
+angular.module('design').service('DesignSrv',
+	 [
+	 	'$http', 
+	 	'Restangular',
+	 	'CommonSrv',
+	 	function ($http , Restangular, CommonSrv) {
+			// Now set up the states
+			var self = this;
+			/**
+				Sends a post request to the backend.
+			**/
+			self.saveDesign = function(userId, data, success, error){
+				var formData = CommonSrv.getFormData(data);
+				console.log('design ---------  ' , data);
+				console.log(formData);
+				
+				Restangular.one(userId.toString()).one('designs').withHttpConfig({
+					transformRequest: angular.identity
+				}).customPOST(formData, '', undefined, {
+					'Content-Type': undefined
+				}).then(success);
+			}
 
-	self.getUserDesign = function(userId, params, success, error){
+			self.getUserDesign = function(userId, params, success, error){
 
-	}
-
-
-}]);
+			}
+		}
+	]);
 
 })(angular);
--- app/scripts/overview/OverviewCtrl.js
+++ app/scripts/overview/OverviewCtrl.js
@@ -3,26 +3,29 @@
 **/
 'use strict';
 (function(angular){
-  angular.module('overview').controller('OverviewCtrl',
-    [
+angular.module('overview').controller('OverviewCtrl',
+	[
 		'$scope',
 		'Restangular',		
 		function($scope, Restangular{
-		Restangular.all('template').getList().then(function(result){
-			/*console.log('My data ------------- ',result[0].id);	
-			console.log('My data ------------- ',result[0].screenshots);	
-			console.log('My data ------------- ',result[0].title);	
-			console.log('My data ------------- ',result[0].age);	
-			console.log('My data ------------- ',result[0].category);*/
-			$scope.templates = result;	
-			console.log("DATA ----------------- ",$scope.templates);
-	    });
-	    $scope.subheader = $scope.$parent.subheader;
-	    $scope.subheader.title = 'Overview';
-		$scope.isHidden = false;    
-		$scope.repeatRow = [{"name" : "User Created" },{"name" : "Featured"},{"name" : "Music"}];  
-    }]);
+			Restangular.all('template').getList().then(function(result){
+				/*console.log('My data ------------- ',result[0].id);	
+				console.log('My data ------------- ',result[0].screenshots);	
+				console.log('My data ------------- ',result[0].title);	
+				console.log('My data ------------- ',result[0].age);	
+				console.log('My data ------------- ',result[0].category);*/
+				$scope.templates = result;	
+				console.log("DATA ----------------- ",$scope.templates);
+		    });
+		    
+		    $scope.subheader = $scope.$parent.subheader;
+		    $scope.subheader.title = 'Overview';
+			$scope.isHidden = false;    
+			$scope.repeatRow = [{"name" : "User Created" },{"name" : "Featured"},{"name" : "Music"}];  
+		}
+	]);
 })(angular);
 
 
 
+	
