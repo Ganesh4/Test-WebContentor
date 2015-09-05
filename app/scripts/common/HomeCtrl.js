6c0da6a17af53152e5374af1321e204c1a3c447e
Ganesh4 <ughadeganesh@gmail.com>
2015-09-05 18:41:52 +0530

    Indentation Added

--- app/scripts/common/HomeCtrl.js
+++ app/scripts/common/HomeCtrl.js
@@ -2,24 +2,23 @@
 'use strict';
 
 (function(angular){
+angular.module('microsite').controller('HomeCtrl',
+    [
+        '$scope',
+        '$state',
+        'ApiSrv',
+        function($scope ,$state, ApiSrv){
 
-  	angular.module('microsite').controller('HomeCtrl',
-    		['$scope',
-    		'$state',
-            'ApiSrv',
-    		 function($scope ,$state, ApiSrv){
-    			
-    				$scope.subheader ={
-    					title : 'Overview',
-    					//APPLY_BTN : 'refresh' 
-    					} 
-    						$scope.reload = function(){
-    							//$scope.$event = $state.current; 
-    							console.log('state ------------ ',$state);
-    							$state.reload();
-    							//console.log('$event',$scope.$event);
-
-							}
-	    			}
-    		]);
-	})(angular);
+            $scope.subheader ={
+            title : 'Overview',
+            //APPLY_BTN : 'refresh' 
+            } 
+            $scope.reload = function(){
+                //$scope.$event = $state.current; 
+                console.log('state ------------ ',$state);
+                $state.reload();
+                //console.log('$event',$scope.$event);
+            }
+        }
+    ]);
+})(angular);
