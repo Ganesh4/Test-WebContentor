fa4520a850ace16d7184ad3531b3c6b466dc54b1
subash0785 <subash@fusioninfosys.com>
2015-09-08 18:44:09 +0530

    design controller js add

---
+++ app/scripts/design/DesignDetailCtrl.js
@@ -0,0 +1,19 @@
+/**
+*
+**/
+'use strict';
+(function(angular){
+angular.module('design').controller('DesignDetailCtrl',
+	[
+		'$scope',
+		'Restangular',
+		'OverviewApiSrv',
+		'CommonSrv',
+		function($scope, Restangular, OverviewApiSrv, CommonSrv){
+			$scope.subheader.title = 'Design Details';
+			console.log('Selected Template -------------- ',$scope);
+			
+					
+		}
+	]);
+})(angular);
