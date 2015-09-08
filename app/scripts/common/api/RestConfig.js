4a6c2afc8fee2e0d001225304754e35a805daf65
Ganesh4 <ughadeganesh@gmail.com>
2015-09-08 12:24:15 +0530

    restconfig updated

--- app/scripts/common/api/RestConfig.js
+++ app/scripts/common/api/RestConfig.js
@@ -7,7 +7,7 @@
 	'RestangularProvider',
 	function($urlProvider, $stateProvider, RestangularProvider){
 		
-		RestangularProvider.setBaseUrl('http://localhost:8080/MicroS/');
+		RestangularProvider.setBaseUrl('http://localhost/MicroS/');
 		
 
 	}]);
