e06b17e8cfb9d33d0826a2f96858fbfd31992ec2
Ganesh4 <ughadeganesh@gmail.com>
2015-09-11 19:28:42 +0530

    APP Module Added

---
+++ app/scripts/app/AppCtrl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function(angular){
+
+	angular.module('app').controller('AppCtrl',
+		[
+			'$scope',
+			'$state',
+			'ApiSrv',
+       		'CommonSrv',
+			function($scope,$state, ApiSrv,CommonSrv){
+					
+			}
+		]);
+
+})(angular);
