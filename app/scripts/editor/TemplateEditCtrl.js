37f298608a679ea0d16e4cba4cccd693f331e006
subash0785 <subash@fusioninfosys.com>
2015-09-15 18:38:32 +0530

    add menu on editor page

--- app/scripts/editor/TemplateEditCtrl.js
+++ app/scripts/editor/TemplateEditCtrl.js
@@ -26,6 +26,80 @@
 						});
 					}
 				});
+					$scope.showSubMenu = function(mainMenu){
+					
+					var subMenuItem = [];
+					if(mainMenu == 'add'){
+						var item_01 = {
+							name : 'Section'	
+						}
+
+						var item_02 = {
+							name : 'Images'
+						}
+						var item_03 = {
+							name : 'Video'
+						}
+						var item_04 = {
+							name : 'Icons'
+						}
+						var item_05 = {
+							name : 'Button'
+						}
+						var item_06 = {
+							name : 'Graph'
+						}
+						var item_07 = {
+							name : 'Shapes'
+						}
+						var item_08 = {
+							name : 'Side Navigation'
+						}
+						var item_09 = {
+							name : 'Slider'
+						}
+
+						$scope.isAddMenu = true;
+						subMenuItem = [item_01,item_02,item_03,item_04,item_05,item_06,item_07,item_08,item_09];
+					}else if (mainMenu == 'section'){
+						var item_01 = {
+							name : 'Problem'	
+						}
+
+						var item_02 = {
+							name : 'Solution'
+						}
+						var item_03 = {
+							name : 'Our Team'
+						}
+						subMenuItem = [item_01,item_02,item_03];
+
+					}else if (mainMenu == 'widgets'){
+						var item_01 = {
+							name : 'Apps'	
+						}
+
+						var item_02 = {
+							name : 'Facebook'
+						}
+						var item_03 = {
+							name : 'Twitter'
+						}
+						var item_04 = {
+							name : 'Linkedin'
+						}
+						subMenuItem = [item_01,item_02,item_03,item_04];
+					}
+
+
+					$scope.listSubMenu(subMenuItem);
+					
+				}
+
+				$scope.listSubMenu = function(data){
+					$scope.subMenu = data;
+					//$('')
+				}
 			}
 		]
 )})(angular);
