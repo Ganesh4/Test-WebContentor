3b521f28d791fbf0ac546f9c8b75f1a9a03fc0aa
prakash-webcontentor <prakash@webcontentor.com>
2015-09-21 13:01:57 +0530

    new changes

--- app/module-config.js
+++ app/module-config.js
@@ -6,15 +6,13 @@
  */
 (function(angular){
 
-	angular.module('vendor',['ui.router','restangular', 'validation','validation.rule','ui.select2','ngSanitize','cb.x2js','froala']);
+	angular.module('vendor',['ui.router','restangular', 'validation','validation.rule','ui.select2','ngSanitize']);
 
 	angular.module('common',['vendor','angularUtils.directives.uiBreadcrumbs']);
 	
 	angular.module('assets',['common']);
 
-	angular.module('app',['assets']);
-
-	angular.module('home',['app']);
+	angular.module('home',['common','assets']);
 
 	angular.module('overview',['home']);
    
