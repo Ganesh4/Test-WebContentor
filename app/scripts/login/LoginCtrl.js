c0407a77209c224706787aad3f846ae6f87154da
subash0785 <subash@fusioninfosys.com>
2015-09-24 20:44:34 +0530

    Login UI

--- app/scripts/login/LoginCtrl.js
+++ app/scripts/login/LoginCtrl.js
@@ -1,1 +1,24 @@
-LoginCtrl.js
+'use strict';
+
+(function(angular){
+
+
+    angular.module('login')
+        .controller('LoginCtrl',[
+        	'$scope',
+        	'loginServ',
+        	'$state',
+        	function($scope,loginServ,$state){
+            	$scope.uname='';
+            	$scope.password='';
+            	$scope.loginFun = function (uname, password) {
+                $scope.loginStaus= loginServ.loginCheck(uname,password);
+                if($scope.loginStaus) {
+                    $state.go('app.home.compaign');
+                	}else{
+                  		$scope.message='Invalid User Name or Password';
+                	}
+                }
+            }
+        ])
+    })(angular)
