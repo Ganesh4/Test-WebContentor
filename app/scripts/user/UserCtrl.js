f20e074eddbff12beb8f6e90690c32a2e8ab13d3
subash0785 <subash@fusioninfosys.com>
2015-09-25 13:05:46 +0530

    user module

---
+++ app/scripts/user/UserCtrl.js
@@ -0,0 +1,16 @@
+/*
+*
+*/
+'use strict';
+(function(angular){
+angular.module('user').controller('UserCtrl',
+    [
+        '$scope',
+        '$state',
+        'ApiSrv',
+        'CommonSrv',
+        function($scope ,$state, ApiSrv,CommonSrv){
+            
+        }
+    ]);
+})(angular);
