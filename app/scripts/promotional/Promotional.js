8e68d937a211cbb64ecdf3d6d3f4c4dcebe0a20b
subash0785 <subash@fusioninfosys.com>
2015-10-15 20:07:43 +0530

    promotional page

---
+++ app/scripts/promotional/Promotional.js
@@ -0,0 +1,54 @@
+/**
+ * 
+ *
+ *
+ */
+'use strict';
+
+(function(angular){
+	angular.module('promotional').config(
+		[
+		'$urlRouterProvider',
+		'$stateProvider',
+		function($urlProvider,$stateProvider){
+		   	$stateProvider.state('app.home.campaign.promotional', {
+	                url: '/promotional',
+	                templateUrl:'views/promotional/promotional.html',
+	                controller:'PromotionalCtrl',
+                data: {
+	                displayName: 'Promotional',
+	                LeftNavList:[{
+                            icon : 'fa fa-angle-down',
+                            name : 'Properties',
+                            menu : [{
+                                        icon : 'fa fa-life-ring',
+                                        name : 'General',
+                                        state:'app.home.campaign.general'
+                                    },{
+                                        icon : 'fa fa-envelope',
+                                        name : 'Emails',
+                                        state: 'app.home.campaign.emails'
+                                    },{
+                                        icon : 'fa fa-list-alt',
+                                         name : 'Promotional Pages',
+                                         state : 'app.home.campaign.promotional'
+                                    }]
+                                 }
+                            ],
+                    actionBarBtn: [{
+                                    name : "Delete",
+                                    onClickEvent : '',
+                               },{
+                                    name : "Create",
+                                    onClickEvent : '',
+                                
+                               },{
+                                    name : "Save",
+                                    onClickEvent : ''
+                     }],
+                 }
+
+             });
+	     }]);
+
+    })(angular);
