7bc667ee083286c740038e1249a16d006177d4bf
vipul031989 <vipul@webcontentor.com>
2015-10-30 11:29:34 +0530

    Bug Fixed Wc Fields.

--- app/scripts/assets/WcContextMenu.js
+++ app/scripts/assets/WcContextMenu.js
@@ -2,7 +2,7 @@
 
 (function(angular){
 
-angular.module('microsite').directive('wcSectionContextMenu',function($compile){
+angular.module('microsite').directive('wcContextMenu',function($compile){
 		return{
 			restrict:'AE',
 			link : function(scope, element, attrs){
